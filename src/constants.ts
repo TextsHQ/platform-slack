242cba0423c2f632835b00574a2dc5bae4e0a2db
Emilio Villagran <emilio@villagran.cl>
2021-05-03 12:47:30 -0400

    feat(constants): add thread mentions regex

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const EMOTE_REGEX = /:([A-Za-z0-9_]+):/g
 export const MENTION_REGEX = /<@([A-Za-z0-9_]+)>/g
+export const SLACK_THREAD_REGEX = /<#([A-Za-z0-9_]+)\|([A-Za-z0-9_]+)>/g
 export const LINK_REGEX = /<((https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}))>/g
