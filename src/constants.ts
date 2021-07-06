9e4c59581da8accfb7bfcc8a6eb366d7ec2d6780
Emilio Villagran <emilio@villagran.cl>
2021-07-06 11:57:32 -0400

    fix: regex

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const EMOTE_REGEX = /:([+A-Za-z0-9_]+):/g
+export const BOLD_REGEX = /\*[^\s]([+A-Za-z0-9_ ]+[^\s])\*/g
 export const MENTION_REGEX = /<@([A-Za-z0-9_]+)>/g
 export const SLACK_THREAD_REGEX = /<#([A-Za-z0-9_]+)\|([A-Za-z0-9_]+)>/g
-export const LINK_REGEX = /<((https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}))>/g
+export const LINK_REGEX = /<((https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}))(\|[a-zA-Z0-9\- ]+)?>/g
