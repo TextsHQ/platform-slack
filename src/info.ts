61aded730d8e7fce6fdd40901738ca50766a0a91
Emilio Villagran <emilio@villagran.cl>
2021-11-07 19:42:29 -0300

    fix(login): script on launch

--- src/info.ts
+++ src/info.ts
@@ -67,6 +67,88 @@ const info: PlatformInfo = {
         }
       }
 
+      // @see https://gist.github.com/benjamingr/0433b52559ad61f6746be786525e97e8
+      function interceptNetworkRequests(ee) {
+        const open = XMLHttpRequest.prototype.open;
+        const send = XMLHttpRequest.prototype.send;
+
+        const isRegularXHR = open.toString().indexOf("native code") !== -1;
+        // don't hijack if already hijacked - this will mess up with frameworks like Angular with zones
+        // we work if we load first there which we can.
+        if (isRegularXHR) {
+          XMLHttpRequest.prototype.open = function () {
+            ee.onOpen && ee.onOpen(this, arguments);
+      
+            if (ee.onLoad) this.addEventListener("load", ee.onLoad.bind(ee));
+            if (ee.onError) this.addEventListener("error", ee.onError.bind(ee));
+      
+            return open.apply(this, arguments);
+          };
+          XMLHttpRequest.prototype.send = function () {
+            ee.onSend && ee.onSend(this, arguments);
+            return send.apply(this, arguments);
+          };
+        }
+
+        const fetch = window.fetch || "";
+        // don't hijack twice, if fetch is built with XHR no need to decorate, if already hijacked
+        // then this is dangerous and we opt out
+        const isFetchNative = fetch.toString().indexOf("native code") !== -1;
+        if (isFetchNative) {
+          window.fetch = function () {
+            ee.onFetch && ee.onFetch(arguments);
+            const p = fetch.apply(this, arguments);
+            p.then(ee.onFetchResponse, ee.onFetchError);
+            return p;
+          };
+          // at the moment, we don't listen to streams which are likely video
+          const json = Response.prototype.json;
+          const text = Response.prototype.text;
+          const blob = Response.prototype.blob;
+          Response.prototype.json = function () {
+            const p = json.apply(this.arguments);
+            p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "json"));
+            return p;
+          };
+          Response.prototype.text = function () {
+            const p = text.apply(this.arguments);
+            p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "text"));
+            return p;
+          };
+          Response.prototype.blob = function () {
+            const p = blob.apply(this.arguments);
+            p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "blob"));
+            return p;
+          };
+        }
+        return ee;
+      }
+
+      const interceptedResponse = (value) => {      
+        value.onload = function () {
+          if (value.readyState == XMLHttpRequest.DONE) {
+            // Example: https://slack.com/api/signin.findWorkspaces?_x_id=noversion-1636318402.702&slack_route=T00000000&_x_version_ts=no-version
+            if (!value?.responseURL?.includes("api/signin.findWorkspaces")) return;
+      
+            const data = value?.response ? JSON.parse(value.response) : {};
+
+            if (data?.current_teams?.length === 1) {
+              const [team] = data?.current_teams;
+              window.__loginReturnValue.magicLink = team.magic_login_url;
+              setTimeout(() => window.close(), 1000);
+              return;
+            }
+          }
+        };
+      };
+
+      interceptNetworkRequests({
+        onFetch: interceptedResponse,
+        onFetchResponse: interceptedResponse,
+        onSend: interceptedResponse,
+        onError: console.log,
+      });
+      
       window.__addedListener = false
       window.__addNavigationListener = window.__addNavigationListener || function() {
         if (window.__addedListener) return
@@ -83,6 +165,88 @@ const info: PlatformInfo = {
         window.__loginReturnValue.magicLink = href
         setTimeout(() => window.close(), 1000)
       }
+
+      // @see https://gist.github.com/benjamingr/0433b52559ad61f6746be786525e97e8
+      function interceptNetworkRequests(ee) {
+        const open = XMLHttpRequest.prototype.open;
+        const send = XMLHttpRequest.prototype.send;
+
+        const isRegularXHR = open.toString().indexOf("native code") !== -1;
+        // don't hijack if already hijacked - this will mess up with frameworks like Angular with zones
+        // we work if we load first there which we can.
+        if (isRegularXHR) {
+          XMLHttpRequest.prototype.open = function () {
+            ee.onOpen && ee.onOpen(this, arguments);
+      
+            if (ee.onLoad) this.addEventListener("load", ee.onLoad.bind(ee));
+            if (ee.onError) this.addEventListener("error", ee.onError.bind(ee));
+      
+            return open.apply(this, arguments);
+          };
+          XMLHttpRequest.prototype.send = function () {
+            ee.onSend && ee.onSend(this, arguments);
+            return send.apply(this, arguments);
+          };
+        }
+
+        const fetch = window.fetch || "";
+        // don't hijack twice, if fetch is built with XHR no need to decorate, if already hijacked
+        // then this is dangerous and we opt out
+        const isFetchNative = fetch.toString().indexOf("native code") !== -1;
+        if (isFetchNative) {
+          window.fetch = function () {
+            ee.onFetch && ee.onFetch(arguments);
+            const p = fetch.apply(this, arguments);
+            p.then(ee.onFetchResponse, ee.onFetchError);
+            return p;
+          };
+          // at the moment, we don't listen to streams which are likely video
+          const json = Response.prototype.json;
+          const text = Response.prototype.text;
+          const blob = Response.prototype.blob;
+          Response.prototype.json = function () {
+            const p = json.apply(this.arguments);
+            p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "json"));
+            return p;
+          };
+          Response.prototype.text = function () {
+            const p = text.apply(this.arguments);
+            p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "text"));
+            return p;
+          };
+          Response.prototype.blob = function () {
+            const p = blob.apply(this.arguments);
+            p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "blob"));
+            return p;
+          };
+        }
+        return ee;
+      }
+
+      const interceptedResponse = (value) => {      
+        value.onload = function () {
+          if (value.readyState == XMLHttpRequest.DONE) {
+            // Example: https://slack.com/api/signin.findWorkspaces?_x_id=noversion-1636318402.702&slack_route=T00000000&_x_version_ts=no-version
+            if (!value?.responseURL?.includes("api/signin.findWorkspaces")) return;
+      
+            const data = value?.response ? JSON.parse(value.response) : {};
+
+            if (data?.current_teams?.length === 1) {
+              const [team] = data?.current_teams;
+              window.__loginReturnValue.magicLink = team.magic_login_url;
+              setTimeout(() => window.close(), 1000);
+              return;
+            }
+          }
+        };
+      };
+
+      interceptNetworkRequests({
+        onFetch: interceptedResponse,
+        onFetchResponse: interceptedResponse,
+        onSend: interceptedResponse,
+        onError: console.log,
+      });
 
       const addEventsListeners = () => {
         window.addEventListener('hashchange', function(){
