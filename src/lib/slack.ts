6c20f5b11629893b0977f8f090f1564ee11028cd
emilio <emilio@villagran.cl>
2022-07-15 11:16:18 -0400

    fix(threads): participants with undefined as id (#23)

--- src/lib/slack.ts
+++ src/lib/slack.ts
@@ -148,7 +148,7 @@ export default class SlackAPI {
     // This filter is because sometimes the latest message hasn't timestamp and can be a response from a thread
     // so this way we filter only messages that aren't thread responses
     thread.messages = [channel?.latest].filter(x => x?.ts && !x?.thread_ts) || []
-    thread.participants = (thread?.is_im || thread?.is_shared) ? [user] : []
+    thread.participants = thread?.is_im ? [user] : []
     // For some reason groups come with the name 'mpdm-firstuser--seconduser--thirduser-1'
     thread.name = thread?.is_mpim ? thread?.name.replace('mpdm-', '').replace('-1', '').split('--').join(', ') : ''
     timer.timeEnd()
