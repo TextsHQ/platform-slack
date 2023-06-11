57f9668cee2ebda5f23b69373e0ff17cccc23ee7
Emilio Villagran <emilio@villagran.cl>
2023-06-11 02:51:40 -0400

    feat(emojis): add functions

--- src/lib/emoji.ts
+++ src/lib/emoji.ts
@@ -1,13 +1,31 @@
 import emojiData from 'unicode-emoji-json'
 
-export const getEmoji = (block: string): string => {
-  const emojiSlug = block.replace(/:/g, '')
-  const [selectedEmoji] = Object.entries(emojiData).find(([, value]: [string, any]) => value.slug === emojiSlug || `face_with_${emojiSlug}` === value.slug || `${emojiSlug}_face` === value.slug) || []
+import slackEmoji from './slack-emoji.json'
 
-  return selectedEmoji || block
+export const getEmojiUrl = (block: string): string => {
+  const emojiSlug = block.replace(/:/g, '')
+  const selectedEmoji = slackEmoji.find(value => value.short_name === emojiSlug)
+
+  if (selectedEmoji) return `https://a.slack-edge.com/production-standard-emoji-assets/14.0/apple-medium/${selectedEmoji.image}`
+
+  return block
+}
+
+export const getEmojiUnicode = (block: string): string => {
+  const emojiSlug = block.replace(/:/g, '')
+  const selectedEmoji = slackEmoji.find(value => value.short_name === emojiSlug)
+
+  if (selectedEmoji) return String.fromCodePoint(parseInt(selectedEmoji.unified, 16))
+
+  return block
 }
 
 export const getSlug = (emoji: string): string => {
   const data = emojiData[emoji]
   return data?.slug || emoji
+}
+
+export const getNativeShortcodeFromBlock = (block: string): string => {
+  const emojiUnicode = getEmojiUnicode(block)
+  return getSlug(emojiUnicode)
 }
