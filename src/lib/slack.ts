731c25a52f938b9925f322bc966f369680769a97
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2023-07-20 14:10:22 -0700

    add more headers for fetching html pages

--- src/lib/slack.ts
+++ src/lib/slack.ts
@@ -68,8 +68,26 @@ export default class SlackAPI {
 
   getMutedChannels = () => this.initialMutedChannels
 
+  static get htmlHeaders() {
+    return {
+      accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+      'accept-language': 'en',
+      'cache-control': 'no-cache',
+      pragma: 'no-cache',
+      'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'document',
+      'sec-fetch-mode': 'navigate',
+      'sec-fetch-site': 'none',
+      'sec-fetch-user': '?1',
+      'upgrade-insecure-requests': '1',
+      'User-Agent': texts.constants.USER_AGENT,
+    }
+  }
+
   private fetchHTML = async (url: string) => {
-    const { statusCode, body: html } = await this.httpClient.requestAsString(url, { cookieJar: this.cookieJar, headers: { 'User-Agent': texts.constants.USER_AGENT } })
+    const { statusCode, body: html } = await this.httpClient.requestAsString(url, { cookieJar: this.cookieJar, headers: SlackAPI.htmlHeaders })
     if (statusCode >= 400) {
       throw Error(`${url} returned status code ${statusCode}`)
     }
@@ -93,7 +111,7 @@ export default class SlackAPI {
   }
 
   private getFirstTeamURL = async () => {
-    const res = await texts.fetch('https://my.slack.com/', { cookieJar: this.cookieJar, headers: { 'User-Agent': texts.constants.USER_AGENT }, method: 'HEAD', followRedirect: false })
+    const res = await texts.fetch('https://my.slack.com/', { cookieJar: this.cookieJar, headers: SlackAPI.htmlHeaders, method: 'HEAD', followRedirect: false })
     const { location } = res.headers
     if (location) return location
     return this.getFirstTeamURLOld()
