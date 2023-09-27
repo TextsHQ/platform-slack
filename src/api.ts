a420fbd9f8e79f8ac4d2abae0213e2e13e7c2ee3
Emilio Villagran <emilio@villagran.cl>
2023-09-26 22:55:37 -0300

    feat: remove hardcoded workspaceURL

--- src/api.ts
+++ src/api.ts
@@ -115,7 +115,6 @@ export default class Slack implements PlatformAPI {
     await this.api.init({
       accountID: this.accountID,
       clientToken: undefined,
-      workspaceURL: 'https://createremote.slack.com/',
     })
 
     await this.afterAuth()
