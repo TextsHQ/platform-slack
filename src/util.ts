5495504ef1f2cc05951be5832c9ab6851bc3f97b
Emilio Villagran <emilio@villagran.cl>
2024-01-06 16:32:15 -0300

    feat(utils): rollback group check

--- src/util.ts
+++ src/util.ts
@@ -15,7 +15,3 @@ export const textsTime = (() =>
 export const isDM = (threadID: string) => (threadID || '').startsWith('D')
 
 export const isChannel = (threadID: string) => (threadID || '').startsWith('C')
-  // @notes
-  // we still need to confirm this, there's no official documentation about this but seems like
-  // groups are considered channels but they have reserved prefix: `C0`.
-  && (threadID || '').startsWith('C0')
